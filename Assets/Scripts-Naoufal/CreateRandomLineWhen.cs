7ad90b514d98608d525bbce39d1aa275d1207f1a
naoufal <naoufal.chaara.5@gmail.com>
2019-08-21 12:52:49 +0200

    Save version

---
+++ Assets/Scripts-Naoufal/CreateRandomLineWhen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandomLineWhen : MonoBehaviour
+{
+
+    public RandomLine m_builder;
+
+    public bool m_createAtStart;
+    public KeyCode m_keyToSwitch = KeyCode.Keypad1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_createAtStart)
+            m_builder.LoadNextLine();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(m_keyToSwitch))
+            m_builder.LoadNextLine();
+
+    }
+}
