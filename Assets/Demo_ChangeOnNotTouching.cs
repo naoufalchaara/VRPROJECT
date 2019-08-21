afa0341c519cc1130da47823fe2f16a4404e0994
Little Big Monkey <info@littlebigmonkey.com>
2019-08-21 13:39:28 +0200

    Test

---
+++ Assets/Demo_ChangeOnNotTouching.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Demo_ChangeOnNotTouching : MonoBehaviour
+{
+    public OverlapCastSelector m_selector;
+
+    public Material m_materialColor;
+
+
+    
+    //public bool m_useTimerToLoadScene;
+    //public float m_countDown=3;
+    //public float m_countDownTimer;
+
+
+    void Update()
+    {
+        bool hasCollision = m_selector.HasCollision();
+        m_materialColor.color = hasCollision ? Color.green : Color.red;
+        //if (hasCollision)
+        //{
+        //    m_countDownTimer = m_countDown;
+        //}
+        //else
+        //{
+        //    m_countDownTimer -= Time.deltaTime ;
+        //    if (m_countDownTimer <= 0)
+        //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //}
+    }
+}
