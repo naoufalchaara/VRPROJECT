148c8756e800ea6c342efd48731316175e72656a
naoufal <naoufal.chaara.5@gmail.com>
2019-08-23 14:38:01 +0200

    -Light + Fire +Event + Timer

--- Assets/Eloi/TestCollider.cs
+++ Assets/Eloi/TestCollider.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class TestCollider : MonoBehaviour
 {
 
     public LayerMask m_mask;
-    public UnityEvent m_onTouch ;
+    public UnityEvent m_onTouch;
     private void OnCollisionEnter(Collision collision)
     {
-        if (IsInLayerMask(collision.gameObject.layer, m_mask)) {
-            Debug.Log("Bou");
-            m_onTouch.Invoke();
-        }
-    }
 
-    public static bool IsInLayerMask(int layer, LayerMask layermask)
-    {
-        return layermask == (layermask | (1 << layer));
+        if (IsInLayerMask(collision.gameObject.layer, m_mask))
+        {
+
+            m_onTouch.Invoke();
     }
+}
+
+public static bool IsInLayerMask(int layer, LayerMask layermask)
+{
+    return layermask == (layermask | (1 << layer));
+}
 }
