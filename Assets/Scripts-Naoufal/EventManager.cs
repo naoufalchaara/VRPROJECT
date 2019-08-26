ccbf8d93a0632f1c7a0653054f5ff52ffb09c64c
naoufal <naoufal.chaara.5@gmail.com>
2019-08-26 17:27:35 +0200

    ->>>>Visual effects

--- Assets/Scripts-Naoufal/EventManager.cs
+++ Assets/Scripts-Naoufal/EventManager.cs
@@ -7,38 +7,49 @@ public class EventManager : MonoBehaviour
 
     public delegate void DesactivateLight(bool lightStateOn);
     public static event DesactivateLight OnClick;
-    // Start is called before the first frame update
-    private void Update()
+    public AudioSource Laugh;
+    public AudioSource Whisper;
+    public AudioSource Electric;
+
+
+
+  
+
+    public  void SendSwitchLightEvent()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (OnClick != null)
         {
-            if (OnClick != null)
-            {
-                StartCoroutine(Switch());
-               // OnClick();
-            }
+            StartCoroutine(Switch());
+            // OnClick();
+            
         }
     }
-
 
     public IEnumerator Switch()
     {
         OnClick(true);
         yield return new WaitForSeconds(1);
+        Electric.Play();
         OnClick(false);
         yield return new WaitForSeconds(0.2f);
         OnClick(true);
         yield return new WaitForSeconds(1.6f);
+        Electric.Play();
+
         bool state = false;
         for (int i = 0; i < 40; i++)
         {
             OnClick(state);
             state = !state;
             yield return new WaitForSeconds(0.1f);
+            
 
         }
         OnClick(false);
-
+        yield return new WaitForSeconds(2f);
+        Laugh.Play();
+        yield return new WaitForSeconds(5f);
+        Whisper.Play();
     }
 
 }
